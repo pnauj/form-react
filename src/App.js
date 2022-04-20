12416c5080f75b1f2a1331f451114bb307109471
pnauj <torres.jpw@gmail.com>
2022-04-20 20:50:01 -0300

    se empezo con la estructura del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./Style.css"
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <form>
+          <label>Usuraio</label>
+          <input type="text" name="usuario"  placeholder="Ingrese su usuario"/>
+          <p>Lorem impsun slorem sooo</p>
+        </form>
+      </main>
+    </>
   );
 }
+
+
+const Formulario = styled.form`
+  
+`;
 
 export default App;
